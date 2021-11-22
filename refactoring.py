a848170d3c9e6e747158c6118e1b0fb46c55b301
Nikita Rodetsky <nrodetckii@mail.ru>
2021-11-22 16:02:41 +0300

    turned text into a list of words

--- refactoring.py
+++ refactoring.py
@@ -1,15 +1,36 @@
-#if '...' in string between two integers
-def pattern_1(string):
-    pass
+def pattern(string):
+    import copy
+    import re
 
-#if '...' in string between string and integer
-def pattern_2(string):
-    pass
+    lst = list(string)
+    list_of_numbers = tuple(map(str, range(10)))
+    list_of_words = []
 
-#if 'До' in string and there is only one integer
-def pattern_3(string):
-    pass
+    i = 0
+    while i < len(lst):
+        #if element is a number
+        if lst[i] in list_of_numbers:
+            start = i
+            while i < len(lst) and lst[i] in list_of_numbers:
+                i += 1
+            end = i
+            list_of_words.append(''.join(lst[start:end]))
+        #if element is a letter
+        elif lst[i] != '\n' and lst[i] != ' ' and lst[i] != '\xa0':
+            start = i
+            while i < len(lst) and not(lst[i] in list_of_numbers) and lst[i] != '\n' and lst[i] != ' ' and lst[i] != '\xa0':
+                i += 1
+            end = i
+            list_of_words.append(''.join(lst[start:end]))
+            i += 1
+        #if element is a ' ' or '\n' or '\xa0' 
+        else:
+            i += 1
+    #then if there is a word merged with '...'
+    
+    return list_of_words
 
-#if there is only one integer
-def pattern_4(string):
-    pass
+with open('test_file') as file:
+    string = file.read()
+
+print(pattern(string))
