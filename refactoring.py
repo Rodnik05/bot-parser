b54eed65f1101dca971945ad15d94f5aec9e0186
Nikita Rodetsky <nrodetckii@mail.ru>
2021-11-22 12:55:09 +0300

    added refactoring file

---
+++ refactoring.py
@@ -0,0 +1,15 @@
+#if '...' in string between two integers
+def pattern_1(string):
+    pass
+
+#if '...' in string between string and integer
+def pattern_2(string):
+    pass
+
+#if 'До' in string and there is only one integer
+def pattern_3(string):
+    pass
+
+#if there is only one integer
+def pattern_4(string):
+    pass
